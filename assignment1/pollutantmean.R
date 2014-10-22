03141b01c21b91eafdd38ae479bda5852df9d08d
Diogo Martins <diogohsm@gmail.com>
2014-10-22 16:24:14 -0200

    Resolvido

--- assignment1/pollutantmean.R
+++ assignment1/pollutantmean.R
@@ -11,4 +11,22 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
+  pollutant_data <- c()
+  files <- as.character( list.files(directory) )
+  file_paths <- paste(directory, files, sep="")
+  for(i in id) {
+    file_loop <- read.csv(file_paths[i], header=T, sep=",")
+    head(file_loop)
+    pollutant
+    na_removed <- file_loop[!is.na(file_loop[, pollutant]), pollutant]
+    pollutant_data <- c(pollutant_data, na_removed)
+  }
+  result <- mean(pollutant_data)
+  return(result) 
 }
+
+#setwd("~/Coursera/1-TDST/assignment1/")
+#round(pollutantmean("./specdata/", "sulfate", 1:100),2) == 2.86
+#round(pollutantmean("./specdata/", "sulfate", 20:30),2) == 1.8
+#round(pollutantmean("./specdata/", "nitrate", 1:100),2) == 2.07
+#round(pollutantmean("./specdata/", "nitrate", 200),2) == 2
